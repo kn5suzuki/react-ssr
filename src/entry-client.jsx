50c7f541f451fc3d9911d682a9049dccfb75aef4
kn5suzuki <suzukikengo@g.ecc.u-tokyo.ac.jp>
2023-10-28 17:45:36 +0900

    initial commit

---
+++ src/entry-client.jsx
@@ -0,0 +1,18 @@
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { App } from './App'
+
+ReactDOM.hydrateRoot(
+  document.getElementById('app'),
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+)
+console.log('hydrated')
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </React.StrictMode>
+// );
